283226eaddb0dd6afeca61baacd920a81bacc668
Steve Sfartz <steve.sfartz@gmail.com>
2016-01-17 16:20:14 +0100

    google app engine helloworld

---
+++ gae/hello.go
@@ -0,0 +1,15 @@
+package gae
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Gopher !")
+}
+
